31edb22029350ddfdd94071d30fdc176bb603d8d
Dezzy12 <desmondneba134@gmail.com>
2023-05-22 09:24:12 +0100

    style: minor changes to the app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:LandingPageComponent},
+  {path: 'home', component:LandingPageComponent},
+  { path: 'auth', loadChildren: () => import('./module/eg-auth/eg-auth.module').then(m => m.EgAuthModule) }, 
+  { path: 'user', loadChildren: () => import('./module/eg-user/eg-user.module').then(m => m.EgUserModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
