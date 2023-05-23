8ed29ff703b9be226580c48f6e9893eb555daaba
Dezzy12 <desmondneba134@gmail.com>
2023-05-23 10:02:12 +0100

    feat: modified auth module

--- src/app/module/eg-auth/eg-auth.module.ts
+++ src/app/module/eg-auth/eg-auth.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { EgAuthRoutingModule } from './eg-auth-routing.module';
 import { EgAuthComponent } from './eg-auth.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 
 @NgModule({
   declarations: [
-    EgAuthComponent
+    EgAuthComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule,
