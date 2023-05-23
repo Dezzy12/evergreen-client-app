539bea7fad11c27004b88867781fa9d42137a254
Dezzy12 <desmondneba134@gmail.com>
2023-05-23 10:04:52 +0100

    feat: modified app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: '', component:LandingPageComponent},
+  {path: '', pathMatch:'full', redirectTo:'/home'},
   {path: 'home', component:LandingPageComponent},
   { path: 'auth', loadChildren: () => import('./module/eg-auth/eg-auth.module').then(m => m.EgAuthModule) }, 
-  { path: 'user', loadChildren: () => import('./module/eg-user/eg-user.module').then(m => m.EgUserModule) }];
+  { path: 'user', loadChildren: () => import('./module/eg-user/eg-user.module').then(m => m.EgUserModule) },
+  { path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
