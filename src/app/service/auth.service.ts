5a6e5432061b5e87f4c64dff9a6be9ebc06bfb6d
Dezzy12 <desmondneba134@gmail.com>
2023-05-26 02:59:53 +0100

    feat: implementing service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User, UserLogin, UserResponse } from '../models/user.model';
+import { LocalStorageUtils } from '../utils/local-storage.utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl: string = environment.baseUrl;
+  constructor(private http: HttpClient) {
+    
+    
+   }
+   register(data: User):Observable<UserResponse> {
+    return this.http.post<UserResponse>(`${this.baseUrl}/register/`, data);
+   }
+
+   login = (data: UserLogin): Observable<UserResponse> =>{
+    return this.http.post<UserResponse>(`${this.baseUrl}/login/`, data);   
+  }
+
+  logout = () =>{
+    LocalStorageUtils.deleteToken;
+  }
 }
