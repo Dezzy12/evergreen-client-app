a490bcf53f3c35d4d67f5a26a4cce319eec5d422
Dezzy12 <desmondneba134@gmail.com>
2023-05-27 11:30:35 +0100

    feat: created/ updated models

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,11 @@
+export interface Book{ 
+    book_id: number;
+    title: string;
+    author: string;
+    isbn:number;
+    description:string;
+    category:string;
+    is_available:boolean;
+    image:string;
+    pdf_reference:string;
+}
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,6 +1,7 @@
 export interface User{
     username: string;
     password: string;
+    address: string;
     email: string;
 }
 
