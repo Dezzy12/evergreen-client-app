f3bbe68ddc555f451902569c05db1c9fda31006a
Dezzy12 <desmondneba134@gmail.com>
2023-05-27 11:25:50 +0100

    feat: implementation of book-preview

--- src/app/module/eg-user/components/book-preview/book-preview.component.ts
+++ src/app/module/eg-user/components/book-preview/book-preview.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Book } from 'src/app/models/book.model';
+import { BookService } from 'src/app/service/book.service';
 
 @Component({
   selector: 'app-book-preview',
@@ -7,18 +9,41 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./book-preview.component.css']
 })
 export class BookPreviewComponent implements OnInit {
-  books:any;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+
+  book!: Book;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private bookService: BookService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(params => {
-      const idparams= params.get('id');
+    this.activatedRoute.paramMap.subscribe((params : ParamMap) => {
+      const book_id = params.get('id');
       
-      
-    })
+      if (typeof book_id == 'string') {
+        let id = parseInt(book_id) 
 
-    
+        this.bookService.getBookById(id).subscribe({
+          next: res => {
+            this.book = res
+          },
+          error: err => {
+            // handles error
+            console.log(err)
+          }
+        })
+      }
+    })
   }
+  readnow(): void {
+    this.router.navigate(['readnow']);
+  }
+
+  downloadPdf(url:string, filename:string) {
+    this.bookService.downloadFile(url, filename);
+    }
+  
 
 }
