4e722f4329d1763afc38ee7274657c83f202f53a
Dezzy12 <desmondneba134@gmail.com>
2023-05-27 11:29:57 +0100

    feat:Reading module updated

--- src/app/module/eg-user/components/reading/reading.component.ts
+++ src/app/module/eg-user/components/reading/reading.component.ts
@@ -1,4 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from 'src/app/models/book.model';
+import { BookService } from 'src/app/service/book.service';
 
 @Component({
   selector: 'app-reading',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadingComponent implements OnInit {
 
-  constructor() { }
+  book_id!: any;
+  book!: Book;
+ 
+
+  constructor(
+   private activatedRoute: ActivatedRoute,
+   private router: Router,
+   private bookService: BookService,
+   private location : Location,
+  ) { 
+    this.book_id = this.router.getCurrentNavigation()?.extras.state;
+    
+  }
 
   ngOnInit(): void {
+    // this.book_id=history.state;
+    
+    let id = history.state;
+    // console.log(this.book_id);
+    // const id = (this.location.getState());
+
+   
+    this.bookService.getBookById(id.book_id).subscribe({
+      next: res =>{
+        this.book = res;
+        console.log(res);
+      },
+      error: err =>{
+        console.log(err);
+      }
+      
+    })
   }
+  
 
 }
